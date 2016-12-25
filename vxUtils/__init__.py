1f6af4efe674a1891f067af94246cc7e3800889b
李宝 <libao@vex1023.com>
2016-12-25 23:41:13 +0800

    版本号:0.0.6 增加 @cache

--- vxUtils/__init__.py
+++ vxUtils/__init__.py
@@ -6,7 +6,7 @@ email :
 
 __author__ = 'vex1023'
 __email__ = 'vex1023@qq.com'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 __homepages__ = 'https://github.com/vex1023/vxUtils'
 __logger__ = 'vxQuant.vxUtils'
 
